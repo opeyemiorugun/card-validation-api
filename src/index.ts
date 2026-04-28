a13cbf91e1669bb571793c00b0b6a16627fcbc0d
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2026-04-28 17:33:57 +0100

    refactor: extract Luhn check into validateCardNumber function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-const cardNumber: string = "17893729974";
+function validateCardNumber(cardNumber: string): boolean {
+    let digits: number[] = cardNumber.split('').map(p => Number(p));
+    // Reverse the digits
+    digits.reverse();
+    // Double digits on the odd position
+    for (let i: number = 1; i < digits.length; i += 2) {
+        digits[i]! *= 2;
+    }
+    // Subtract 9 from numbers over 9
+    const adjusted: number[] = digits.map(value => value > 9 ? value - 9 : value);
+    // Sum all the digits
+    const sum: number = adjusted.reduce((acc, n) => acc + n, 0);
+    return sum % 10 === 0;
+}
 
-let digits: number[] = cardNumber.split('').map(p => Number(p));
-
-// Reverse the digits
-digits.reverse();
-// Double digits on the odd position
-for (let i: number = 1; i < digits.length; i+=2) {
-    digits[i]! *= 2;
-}
-const digit: number[] = digits.map(value => value > 9 ? value - 9 : value);
-let sum: number = 0;
-for (const n of digit) {
-    sum += n;
-}
-sum = sum % 10
-if (sum === 0) {
-    console.log("Valid");
-}
-else{
-    console.log("Not Valid");
-}
-console.log(digits);
-console.log(digit);
-console.log(sum);
+export { validateCardNumber };
